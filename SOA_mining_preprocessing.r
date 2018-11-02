19bc5deedc3d42dbe1f3993f6882fba2bbc14489
rungec <c.runge1@uq.edu.au>
2018-11-02 15:00:29 +0100

    changed filters

--- SOA_mining_preprocessing.r
+++ SOA_mining_preprocessing.r
@@ -1,34 +1,39 @@
+#This script summarises the mining database compiled as part of CONNECT "MiningDatabaseOct2018_macro.xlsm"
+
 ### Set up libraries ----
 require(tidyverse)
 require(ggplot2)
 require(readxl)
 
 
-wd <- "D:/Box Sync/Arctic/CONNECT/Paper_5_state_of_arctic/Analysis/Input"
+wd <- "D:/Box Sync/Arctic/CONNECT/Paper_5_state_of_arctic/Intermediate"
 setwd(wd)
 
 #Raw data
-mines <- read_excel("D:/Box Sync/Arctic/Data/Landuse/Mining/Ojas_mining_database_2018/Mining data/MiningDatabaseAug2018_macro.xlsm", sheet="MiningData_merged")
+mines <- read_excel("D:/Box Sync/Arctic/Data/Landuse/Mining/Ojas_mining_database_2018/Mining data/MiningDatabaseOct2018_macro.xlsm", sheet="MiningData_merged")
 
-#Set up data
-mines$Status[mines$Status %in% c("Prefeasibility", "Conceptual", "Feasibility","Bankable", "Under development", "Construction")] <- "Exploration"
-mines$Status[mines$Status %in% c("Operating", "Restart", "Active mine")] <- "Exploitation"
-mines$Status[mines$Status %in% c("Closed", "Suspended", "Care and Maintenance",  "Closed mine")] <- "Closed"
-
+#filter data
 mines <- mines %>% 
           filter(ArcticAmap==1 & Status!="Historic") %>% #select mines in study region, not historic
-          select(Country, Region2, Status, Start, Stop) 
+          filter(MineralGrp!="Industrial minerals" & MineralGrp!="Industrial rock") %>% #drop industrial minerals (~55 mines)
+          filter(Size %in% c(NA, "Large", "Medium", "Medium sized deposit", "Large deposit", "Very large", "Very large deposit", "Unknown", "NA", "Very Large", "Potentially large")) %>% #drop small mines
+          select(Name, Country, Region2, MineralGrp, Status, Start, Stop) 
+
+#Set up data
+mines$Status[mines$Status %in% c("Prefeasibility", "Conceptual", "Feasibility","Bankable")] <- "Exploration"
+mines$Status[mines$Status %in% c("Under development", "Construction", "Operating", "Restart", "Active mine")] <- "Exploitation"
+mines$Status[mines$Status %in% c("Closed", "Suspended", "Care and Maintenance",  "Closed mine")] <- "Closed"
+
 
 #drop rows that are closed, with no closure date
 mines <- mines %>% filter(!(Status=="Closed" &  is.na(Stop)))
+write.csv(mines, "Mining_listofMines_withdates.csv")
 
 #where mines are closed and have no start date, add the start date of 1900
 mines$Start[is.na(mines$Start) & mines$Status=="Closed"] <- 1900
-
 #where mines have no start date, add the start date of 1990
 mines$Start[is.na(mines$Start)] <- 1990
-
-#add stop date of 2017
+#add stop date of 2017 (i.e. where mines are still operating)
 mines$Stop[is.na(mines$Stop)] <- 2017
 
 #Reformat to wide
@@ -46,18 +51,16 @@ for (i in 1:nrow(mines)){
 minesOp <- mineslong %>% 
                 filter(Status %in% c("Closed", "Exploitation")) %>%
                 group_by(Country, Region, year) %>% summarise(nmines=n()) %>%
-                filter(year %in% 1990:2017) %>%
-                spread(year, nmines)
+                filter(year %in% 1900:2017) 
+               # spread(year, nmines)
 write_excel_csv(minesOp, "Mining_nmines_operational_byyear.csv")
 
 #How many mines are under exploitation in each year
 minesExpl <- mineslong %>% 
                 filter(Status=="Exploration") %>%  
                 group_by(Country, Region, year) %>% summarise(nmines=n()) %>%
-                filter(year %in% 1990:2017) %>%
-                spread(year, nmines)
+                filter(year %in% 1900:2017) 
+                #spread(year, nmines)
 write_excel_csv(minesExpl, "Mining_nmines_exploration_byyear.csv")
 
-
-
-
+###
