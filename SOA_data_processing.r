fe94aec5f2eaefce0984b3aba203d4544f95036e
rungec <cru016@uit.no>
2018-11-05 12:51:33 +0100

    fixed col names, added reindeer & mining

--- SOA_data_processing.r
+++ SOA_data_processing.r
@@ -9,19 +9,27 @@ wd <- "D:/Box Sync/Arctic/CONNECT/Paper_5_state_of_arctic/Analysis"
 setwd(wd)
 
 #Ship dist
-shipdist <- read_excel("Input/State_of_Arctic_all_data.xlsx", sheet="shipping_distancesailed")
+shipdist <- read_excel("Input/State_of_Arctic_all_data.xlsx", sheet="shipping_distancesailed", range="A1:J92")
 shipdist$Country <- as.factor(shipdist$Country)
 shipdist$Region <- as.factor(shipdist$region)
 shipdist$Metric <- as.factor(shipdist$ShipCat)
-shipdist_long <- gather(shipdist, key=year, value=value, "2012":"2018")
+shipdist_long <- gather(shipdist, key=year, value=value, "X2012":"X2018")
 write_excel_csv(shipdist_long, "Intermediate/Shipping_distance_long.csv")
+
+#Ship trafficwork
+shipdist <- read_excel("Input/State_of_Arctic_all_data.xlsx", sheet="shipping_trafficwork", range="A1:H3676")
+shipdist$Country <- as.factor(shipdist$Country)
+shipdist$Region <- as.factor(shipdist$region)
+shipdist$Metric <- as.factor(shipdist$ShipCat)
+shipdist$ShipWt <- as.factor(shipdist$shipweight)
+write_excel_csv(shipdist, "Intermediate/Shipping_distance_long.csv")
 
 #Ship vol
 shipvol <- read_excel("Input/State_of_Arctic_all_data.xlsx", sheet="shipping_voltransported", range="A1:AB26")
 shipvol$Country <- as.factor(shipvol$Country)
 shipvol$Metric <- as.factor(shipvol$Cargo)
 shipvol$Region <- as.factor(shipvol$Region)
-shipvol_long <- gather(shipvol, key=year, value=value, "1991":"2017")
+shipvol_long <- gather(shipvol, key=year, value=value, "X1991":"X2016")
 write_excel_csv(shipvol_long, "Intermediate/Shipping_volume_long.csv")
 
 #population
@@ -68,10 +76,17 @@ wells$Metric <- as.factor(wells$Type)
 wells_long <- gather(wells, key=year, value=value, "1920":"2018")
 write_excel_csv(wells_long, "Intermediate/Oilandgas_wells_long.csv")
 
+#mining
+mines <- read_excel("Input/State_of_Arctic_all_data.xlsx", sheet="mining", range="A1:DQ46")
+mines$Country <- as.factor(mines$Country)
+mines$Region <- as.factor(mines$Region)
+mines$Metric <- as.factor(mines$Metric)
+mines_long <- gather(mines, key=year, value=value, "1900":"2017")
+write_excel_csv(mines_long, "Intermediate/Mining_long.csv")
 
 #Hunting
 #number of hunters
-hunters <- read_excel("Input/State_of_Arctic_all_data.xlsx", sheet="Hunters", range="A1:AK47")
+hunters <- read_excel("Input/State_of_Arctic_all_data.xlsx", sheet="Hunters", range="A1:AK45")
 hunters$Country <- as.factor(hunters$Country)
 hunters$Region <- as.factor(hunters$Region)
 hunters$Metric <- as.factor(hunters$Metric)
@@ -87,3 +102,10 @@ fish$Metric <- as.factor(fish$Metric)
 fish_long <- gather(fish, key=year, value=value, "1950":"2014")
 write_excel_csv(fish_long, "Intermediate/Fishing_long.csv")
 
+#reindeer
+rein <- read_excel("Input/State_of_Arctic_all_data.xlsx", sheet="reindeerherding", range="A1:CB17")
+rein$Country <- as.factor(rein$Country)
+rein$Region <- as.factor(rein$Region)
+rein$Metric <- as.factor(rein$Metric)
+rein_long <- gather(rein, key=year, value=value, "1941":"2017")
+write_excel_csv(rein_long, "Intermediate/Reindeer_long.csv")
