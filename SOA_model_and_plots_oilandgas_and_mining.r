e08e6dbbe73d9804b4c6fd550b7c710c69fb4164
rungec <cru016@uit.no>
2019-03-29 14:54:15 +0100

    added trendline plots for SI

--- SOA_model_and_plots_oilandgas_and_mining.r
+++ SOA_model_and_plots_oilandgas_and_mining.r
@@ -146,15 +146,15 @@ oag_dev <- oag %>% filter(subIndustry=="Oilandgas_Development") %>%
   summarise(nwells=sum(value_raw, na.rm=TRUE)) %>% droplevels() %>%
   mutate(znorm = scale(nwells, center=TRUE, scale=TRUE) %>% as.vector) %>% ungroup() #### Normalisation
 
-mod1 <- gamm(znorm ~ s(year), random=list(Country=~1), correlation=NULL, data=oag_dev, method="REML")
+mod3 <- gamm(znorm ~ s(year), random=list(Country=~1), correlation=NULL, data=oag_dev, method="REML")
 #Add in temporal autocorrelation with 1yr lag
-mod2 <- gamm(znorm ~ s(year), random=list(Country=~1), correlation = corAR1(form = ~ year), data=oag_dev, method="REML")
+mod4 <- gamm(znorm ~ s(year), random=list(Country=~1), correlation = corAR1(form = ~ year), data=oag_dev, method="REML")
 
-modelsummaryfun(i, mod1, mod2, oag_dev)
-bestmodplot(mod2, oag_dev, TRUE, 12, 5)
+modelsummaryfun(i, mod3, mod4, oag_dev)
+bestmodplot(mod3, oag_dev, TRUE, 12, 5)
 
 ##########################
-#Model offshore exploration, all regions except russia 1970-2017 ----
+#Model offshore exploration offshore, all regions except russia 1970-2017 ----
 #excludes data for Faroes & Nunavut as we don't have dates for when these wells were drilled
 i <- "Oilandgas_Exploration_offshore"
 #Set up normalised data
@@ -163,15 +163,15 @@ oag_exp_off <- oag %>% filter(subIndustry=="Oilandgas_Exploration" & Offshore==T
   summarise(nwells=sum(value_raw, na.rm=TRUE)) %>%
   mutate(znorm = scale(nwells, center=TRUE, scale=TRUE) %>% as.vector) %>% ungroup() #### Normalisation
 
-mod1 <- gamm(znorm ~ s(year), random=list(Country=~1), correlation=NULL, data=oag_exp_off, method="REML")
+mod5 <- gamm(znorm ~ s(year), random=list(Country=~1), correlation=NULL, data=oag_exp_off, method="REML")
 #Add in temporal autocorrelation with 1yr lag
-mod2 <- gamm(znorm ~ s(year), random=list(Country=~1), correlation = corAR1(form = ~ year), data=oag_exp_off, method="REML")
+mod6 <- gamm(znorm ~ s(year), random=list(Country=~1), correlation = corAR1(form = ~ year), data=oag_exp_off, method="REML")
 
-modelsummaryfun(i, mod1, mod2, oag_exp_off)
-bestmodplot(mod1, oag_exp_off, TRUE, 9, 7)
+modelsummaryfun(i, mod5, mod6, oag_exp_off)
+bestmodplot(mod5, oag_exp_off, TRUE, 9, 7)
 
 ##########################
-#Model offshore development, all regions  except russia 1970-2017 ----
+#Model offshore development offshore, all regions  except russia 1970-2017 ----
 i <- "Oilandgas_Development_offshore"
 #Set up normalised data
 oag_dev_off <- oag %>% filter(subIndustry=="Oilandgas_Development" & Offshore==TRUE) %>%
@@ -180,28 +180,28 @@ oag_dev_off <- oag %>% filter(subIndustry=="Oilandgas_Development" & Offshore==T
   mutate(znorm = scale(nwells, center=TRUE, scale=TRUE) %>% as.vector) %>% ungroup() #### Normalisation
 
 #only one country, so we drop country from the model
-mod1 <- gamm(znorm ~ s(year), correlation=NULL, data=oag_dev_off, method="REML")
+mod7 <- gamm(znorm ~ s(year), correlation=NULL, data=oag_dev_off, method="REML")
 #Add in temporal autocorrelation with 1yr lag
-mod2 <- gamm(znorm ~ s(year), correlation = corAR1(form = ~ year), data=oag_dev_off, method="REML")
+mod8 <- gamm(znorm ~ s(year), correlation = corAR1(form = ~ year), data=oag_dev_off, method="REML")
 
-modelsummaryfun(i, mod1, mod2, oag_dev_off)
-bestmodplot(mod1, oag_dev_off, TRUE, 9, 7)
+modelsummaryfun(i, mod7, mod8, oag_dev_off)
+bestmodplot(mod7, oag_dev_off, TRUE, 9, 7)
 
 ##########################
 #Model oil and gas exploration, aggregated all regions except russia 1950-2017 ----
 i <- "Oilandgas_Exploration_aggregated"
 #Set up normalised data
 oag_exp_agg <- oag %>% filter(subIndustry=="Oilandgas_Exploration") %>%
-  group_by(year) %>% 
-  summarise(nwells=sum(value_raw, na.rm=TRUE)) %>% ungroup() 
+  group_by(year) %>%
+  summarise(nwells=sum(value_raw, na.rm=TRUE)) %>% ungroup()
 
-mod1 <- gamm(nwells ~ s(year), correlation=NULL, data=oag_exp_agg, method="REML")
+mod9 <- gamm(nwells ~ s(year), correlation=NULL, data=oag_exp_agg, method="REML")
 #Add in temporal autocorrelation with 1yr lag
-mod2 <- gamm(nwells ~ s(year), correlation = corAR1(form = ~ year), data=oag_exp_agg, method="REML")
+mod10 <- gamm(nwells ~ s(year), correlation = corAR1(form = ~ year), data=oag_exp_agg, method="REML")
 
-modelsummaryfun(i, mod1, mod2, oag_exp_agg)
-bestmodplot(mod1, oag_exp_agg, FALSE, 9, 7)
-preds <- predict(mod1$gam, newdata=oag_exp_agg, se=TRUE)
+modelsummaryfun(i, mod9, mod10, oag_exp_agg)
+bestmodplot(mod9, oag_exp_agg, FALSE, 9, 7)
+preds <- predict(mod9$gam, newdata=oag_exp_agg, se=TRUE)
 oag_exp_agg_df <- bind_cols(oag_exp_agg, preds) %>% mutate(subIndustry="Exploration")
 
 ##########################
@@ -212,38 +212,38 @@ oag_dev_agg <- oag %>% filter(subIndustry=="Oilandgas_Development") %>%
   group_by(year) %>% 
   summarise(nwells=sum(value_raw, na.rm=TRUE)) %>% ungroup() 
 
-mod1 <- gamm(nwells ~ s(year), correlation=NULL, data=oag_dev_agg, method="REML")
+mod11 <- gamm(nwells ~ s(year), correlation=NULL, data=oag_dev_agg, method="REML")
 #Add in temporal autocorrelation with 1yr lag
-mod2 <- gamm(nwells ~ s(year), correlation = corAR1(form = ~ year), data=oag_dev_agg, method="REML")
+mod12 <- gamm(nwells ~ s(year), correlation = corAR1(form = ~ year), data=oag_dev_agg, method="REML")
 
-modelsummaryfun(i, mod1, mod2, oag_dev_agg)
-bestmodplot(mod2, oag_exp_agg, FALSE, 9, 7)
-preds <- predict(mod1$gam, newdata=oag_dev_agg, se=TRUE)
+modelsummaryfun(i, mod11, mod12, oag_dev_agg)
+bestmodplot(mod12, oag_exp_agg, FALSE, 9, 7)
+preds <- predict(mod11$gam, newdata=oag_dev_agg, se=TRUE)
 oag_dev_agg_df <- bind_cols(oag_dev_agg, preds) %>% mutate(subIndustry="Development")
 
 ##########################
 #Model oil and gas exploration, aggregated offshore except russia 1950-2017 ----
-i <- "Oilandgas_Exploration_aggregated_offshore"
+ i <- "Oilandgas_Exploration_aggregated_offshore"
 #Set up normalised data
-oag_exp_agg_off <- oag %>% filter(subIndustry=="Oilandgas_Exploration" & Offshore==TRUE) %>%
-  group_by(year) %>% 
-  summarise(nwells=sum(value_raw, na.rm=TRUE)) %>% ungroup() 
-
-mod1 <- gamm(nwells ~ s(year), correlation=NULL, data=oag_exp_agg_off, method="REML")
-preds <- predict(mod1$gam, newdata=oag_exp_agg_off, se=TRUE)
-oag_exp_agg_off_df <- bind_cols(oag_exp_agg_off, preds) %>% mutate(subIndustry="Offshore Exploration")
-
+ oag_exp_agg_off <- oag %>% filter(subIndustry=="Oilandgas_Exploration" & Offshore==TRUE) %>%
+   group_by(year) %>% 
+   summarise(nwells=sum(value_raw, na.rm=TRUE)) %>% ungroup() 
+ 
+mod13 <- gamm(nwells ~ s(year), correlation=NULL, data=oag_exp_agg_off, method="REML")
+preds <- predict(mod13$gam, newdata=oag_exp_agg_off, se=TRUE)
+oag_exp_agg_off_df <- bind_cols(oag_exp_agg_off, preds) %>% mutate(subIndustry="Marine exploration")
+ 
 ##########################
-#Model oil and gas development, aggregated offshore  except russia 1950-2017 ----
-i <- "Oilandgas_Development_aggregated_offshore"
-#Set up normalised data
-oag_dev_agg_off <- oag %>% filter(subIndustry=="Oilandgas_Development" & Offshore==TRUE) %>%
-  group_by(year) %>% 
-  summarise(nwells=sum(value_raw, na.rm=TRUE)) %>% ungroup() 
+# #Model oil and gas development, aggregated offshore  except russia 1950-2017 ----
+ i <- "Oilandgas_Development_aggregated_offshore"
+# #Set up normalised data
+ oag_dev_agg_off <- oag %>% filter(subIndustry=="Oilandgas_Development" & Offshore==TRUE) %>%
+   group_by(year) %>% 
+   summarise(nwells=sum(value_raw, na.rm=TRUE)) %>% ungroup() 
 
-mod1 <- gamm(nwells ~ s(year), correlation=NULL, data=oag_dev_agg_off, method="REML")
-preds <- predict(mod1$gam, newdata=oag_dev_agg_off, se=TRUE)
-oag_dev_agg_off_df <- bind_cols(oag_dev_agg_off, preds) %>% mutate(subIndustry="Offshore Development")
+mod14 <- gamm(nwells ~ s(year), correlation=NULL, data=oag_dev_agg_off, method="REML")
+preds <- predict(mod14$gam, newdata=oag_dev_agg_off, se=TRUE)
+oag_dev_agg_off_df <- bind_cols(oag_dev_agg_off, preds) %>% mutate(subIndustry="Marine development")
 
 
 ##########################
@@ -255,8 +255,8 @@ oag_dev_agg_off_df <- bind_cols(oag_dev_agg_off, preds) %>% mutate(subIndustry="
 oagplotdf <- bind_rows(oag_exp_agg_df, oag_dev_agg_df)
 p <- ggplot(oagplotdf, aes(x=year, y=nwells, fill=subIndustry, group=subIndustry)) +
   geom_area(stat='identity', position='identity', alpha=0.5) +
-  geom_ribbon(aes(ymin=fit-1.96*se.fit, ymax=fit+1.96*se.fit), alpha=0.2, fill="grey30")+
-  geom_line(aes(y=fit, col=subIndustry)) +
+  #geom_ribbon(aes(ymin=fit-1.96*se.fit, ymax=fit+1.96*se.fit), alpha=0.2, fill="grey30")+
+  #geom_line(aes(y=fit, col=subIndustry)) +
   coord_cartesian(xlim=c(1950, 2017), expand=FALSE) + 
   scale_x_continuous(breaks=seq(1960, 2010, 10)) +
   xlab("Year") + ylab("Wells spudded") +
@@ -273,15 +273,15 @@ p <- ggplot(oagplotdf, aes(x=year, y=nwells, fill=subIndustry, group=subIndustry
                       title.hjust = 0.5, label.hjust = 1, nrow = 1,
                       byrow = T, # also the guide needs to be reversed
                       reverse = T, label.position = "bottom"))
-ggsave(filename=paste0("Arctic_Oil_and_Gas_Timeseries.png"), p)
+ggsave(filename=paste0("Arctic_Oil_and_Gas_Timeseries.png"), p, width=7.77, height=4.35)
 
 #Plot number of wells spudded offshore (all regions), overlaid with exploration and development trendlines (plus SI) from models
 #Marine timeseries
 oagplotdfoff <- bind_rows(oag_exp_agg_off_df, oag_dev_agg_off_df)
 p <- ggplot(oagplotdfoff, aes(x=year, y=nwells, fill=subIndustry, group=subIndustry)) +
   geom_area(stat='identity', position='identity', alpha=0.5) +
-  geom_ribbon(aes(ymin=fit-1.96*se.fit, ymax=fit+1.96*se.fit), alpha=0.2, fill="grey30")+
-  geom_line(aes(y=fit, col=subIndustry)) +
+  #geom_ribbon(aes(ymin=fit-1.96*se.fit, ymax=fit+1.96*se.fit), alpha=0.2, fill="grey30")+
+  #geom_line(aes(y=fit, col=subIndustry)) +
   coord_cartesian(xlim=c(1970, 2017), expand=FALSE) + 
   scale_x_continuous(breaks=seq(1970, 2010, 10)) +
   xlab("Year") + ylab("Wells spudded") +
@@ -299,9 +299,63 @@ p <- ggplot(oagplotdfoff, aes(x=year, y=nwells, fill=subIndustry, group=subIndus
                       byrow = T, # also the guide needs to be reversed
                       reverse = T, label.position = "bottom"))
 
-ggsave(filename=paste0("Arctic_Oil_and_Gas_Timeseries_marine.png"), p)
+ggsave(filename=paste0("Arctic_Oil_and_Gas_Timeseries_marine.png"), p, width=7.77, height=4.35)
 
+##########################
+# #Set up data aggregated all regions  except russia 1950-2017 ----
+# oag_agg <- oag %>% 
+#   group_by(subIndustry, year) %>% 
+#   summarise(nwells=sum(value_raw, na.rm=TRUE)) %>% ungroup() %>% 
+#   mutate(industry=case_when(subIndustry=="Oilandgas_Development"~"Development",
+#                             subIndustry=="Oilandgas_Exploration"~"Exploration")) %>% ungroup()
+# 
+# #Set up data aggregated offshore  except russia 1950-2017 ----
+# oag_agg_off <- oag %>% filter(Offshore==TRUE) %>%
+#   group_by(subIndustry, year) %>% 
+#   summarise(nwells=sum(value_raw, na.rm=TRUE)) %>% ungroup() %>% 
+#   mutate(industry=case_when(subIndustry=="Oilandgas_Development"~"Marine development",
+#                             subIndustry=="Oilandgas_Exploration"~"Marine exploration"))
 
+#Plot normalised number of wells spudded (all regions), overlaid with exploration and development trendlines (plus SI) from models
+#Set up predictions for trendlines estimated with country as random effect
+#predict oag exploration
+preds1 <- predict(mod2$gam, newdata=oag_exp, se=TRUE) 
+preds1 <- bind_cols(oag_exp, preds1) %>% mutate(industry="Exploration")
+#predict oag development
+preds3 <- predict(mod3$gam, newdata=oag_dev, se=TRUE) 
+preds3 <- bind_cols(oag_dev, preds3) %>% mutate(industry="Development")
+#predict oag exploration offshore
+preds5 <- predict(mod5$gam, newdata=oag_exp_off, se=TRUE) 
+preds5 <- bind_cols(oag_exp_off, preds5) %>% mutate(industry="Marine exploration")
+#predict oag development offshore
+preds7 <- predict(mod7$gam, newdata=oag_dev_off, se=TRUE) 
+preds7 <- bind_cols(oag_dev_off, preds7) %>% mutate(industry="Marine development")
+
+oagplotdf <- bind_rows(preds1, preds3)
+oagplotdf <- bind_rows(oagplotdf, preds5)
+oagplotdf <- bind_rows(oagplotdf, preds7)
+
+#plot exploration vs development time series
+p <- ggplot(oagplotdf, aes(x=year, y=znorm, fill=industry, group=industry)) +
+  geom_ribbon(aes(ymin=fit-1.96*se.fit, ymax=fit+1.96*se.fit), alpha=0.5)+
+  geom_line(aes(y=fit, col=industry)) +
+  coord_cartesian(xlim=c(1950, 2017), expand=FALSE) + 
+  scale_x_continuous(breaks=seq(1960, 2010, 10)) +
+  xlab("Year") + ylab("znorm") +
+  theme_minimal() +
+  theme(legend.position="bottom") +
+  scale_color_manual(values = viridisLite::magma(6)[2:5], guide = "none") +
+  scale_fill_manual(values = viridisLite::magma(6)[2:5],
+                    name = element_blank(),
+                    guide = guide_legend(
+                      direction = "horizontal",
+                      keyheight = unit(3, units = "mm"),
+                      keywidth = unit(30 / length(labels), units = "mm"),
+                      title.position = 'top',
+                      title.hjust = 0.5, label.hjust = 1, nrow = 1,
+                      byrow = T, # also the guide needs to be reversed
+                      reverse = F, label.position = "bottom"))
+ggsave(filename=paste0("Arctic_Oil_and_Gas_Timeseries_znorm.png"), p, width=7, height=5)
 
 ##########################
 # MINING Models ----
@@ -369,7 +423,7 @@ p <-  preds %>%
   geom_ribbon(aes(ymin=fit-1.96*se.fit, ymax=fit+1.96*se.fit), alpha=0.2, fill="grey30")+
   geom_line(aes(y=fit), col="black") +
   theme(legend.position="none") +
-  theme_minimal(18)
+  theme_minimal(24)
 ggsave(filename=paste0(i, "_timeseries.png"), p, width = 9, height=7)
 
 ##########################
@@ -403,8 +457,8 @@ preds_all <- bind_rows(preds_all, preds_russ)
 #Plot mining trends ----
 p <- ggplot(preds_all, aes(x=year, y=nmines, fill=Country, group=Country)) +
   geom_area(stat='identity', position='identity', alpha=0.5) +
-  geom_ribbon(aes(ymin=fit-1.96*se.fit, ymax=fit+1.96*se.fit), alpha=0.2, fill="grey30")+
-  geom_line(aes(y=fit, col=Country)) +
+  #geom_ribbon(aes(ymin=fit-1.96*se.fit, ymax=fit+1.96*se.fit), alpha=0.2, fill="grey30")+
+  #geom_line(aes(y=fit, col=Country)) +
   coord_cartesian(xlim=c(1950, 2017), expand=FALSE) + 
   scale_x_continuous(breaks=seq(1960, 2010, 10)) +
   xlab("Year") + ylab("Number of mines in operation") +
